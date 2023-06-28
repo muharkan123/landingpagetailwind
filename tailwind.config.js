31c0cf2deda3aae4cc0d21c124c9c4c8ef0375d7
arkanwicaksono1602@gmail.com <arkanwicaksono1602@gmail.com>
2023-06-28 11:25:38 +0700

    next

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
         abu : '#374151',
         merah2: '#ef4444',
         secondary : '#1d4ed8',
-        merah3: '#6e0f2c'
+        merah3: '#6e0f2c',
+        puth: '#cbd5e1'
       },
       screens : {
         '2xl' : '1320px',
